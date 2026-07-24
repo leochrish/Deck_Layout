b9b7f9b42fd075ff5c79ca5a1002e3dd9fd4a590
Leo <leoleeu14a1352@gmail.com>
2026-07-24 23:17:24 +0530

    Pipeline changes

--- HorizontalDeck/build.gradle.kts
+++ HorizontalDeck/build.gradle.kts
@@ -58,7 +58,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "io.github.leochrish"
             artifactId = "horizontal-deck"
-            version = "1.0.1"
+            version = "1.0.2"
 
             afterEvaluate {
                 from(components["release"])
